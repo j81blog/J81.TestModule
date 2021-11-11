ce9ce30e2a92f2dda02a24b5a24bb75bc2803dd4
John B <john@j81.nl>
2021-11-11 20:27:41 +0100

    Test

--- ci/Install.ps1
+++ ci/Install.ps1
@@ -48,7 +48,7 @@ Import-Module $manifestPath -Force
 
 # Install packages
 
-if (-Not ($packageProvider = Get-PackageProvider -ListAvailable | Where-Object {$_.Name -like "Nuget" -and $_.Version -ge $([Version]"2.8.5.208")})) {
+if (-Not ($packageProvider = Get-PackageProvider -ListAvailable | Where-Object {$_.Name -like "Nuget" -and $_.Version -ge $([System.Version]"2.8.5.208")})) {
     Install-PackageProvider -Name NuGet -MinimumVersion "2.8.5.208"
 }
 
@@ -58,7 +58,7 @@ If (Get-PSRepository -Name PSGallery | Where-Object { $_.InstallationPolicy -ne 
 }
 $Modules = @("Pester", "PSScriptAnalyzer", "posh-git", "MarkdownPS")
 ForEach ($Module in $Modules) {
-    If ([System.Version]((Find-Module -Name $Module).Version) -gt (Get-Module -Name $Module -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1).Version) {
+    If ([System.Version]((Find-Module -Name $Module | Sort-Object -Property Version -Descending | Select-Object -First 1).Version) -gt [System.Version](Get-Module -Name $Module -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1).Version) {
         Write-Host "Checking module $Module." -ForegroundColor Cyan
         Install-Module -Name $Module -SkipPublisherCheck -Force
         Import-Module -Name $Module -Force
